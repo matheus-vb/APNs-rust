7dffa145133b0307462cfe13809dfae1a46b532c
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 00:37:56 -0300

    feature: add api handler to server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 mod api;
+mod apnservice;
 
-use actix_web::{web, App, HttpServer};
-use crate::api::handle;
+use std::env;
+use dotenv::dotenv;
+use actix_web::{ App, HttpServer };
+use api::trigger;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let address = format!("0.0.0.0:{}", port);
+
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(handle))
+            .service(trigger)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(address)?
     .run()
     .await
 }
