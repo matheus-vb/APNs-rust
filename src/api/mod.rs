e6f526e35f96474a0e2c652259e352fde329b5f3
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 19:11:06 -0300

    chore: refactor api to inject client

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,6 @@
 use actix_web::{web, HttpResponse, Responder, get};
 use serde::Serialize;
-use crate::apnservice::send_notification;
+use crate::apnservice::{send_notification, setup_client};
 
 #[derive(Serialize)]
 struct ApiResponse {
@@ -9,7 +9,12 @@ struct ApiResponse {
 
 #[get("/trigger/{device_token}")]
 pub async fn trigger(device_token: web::Path<String>) -> impl Responder {
-    match send_notification(device_token.as_str()).await {
+    let client = match setup_client() {
+        Ok(client) => client,
+        Err(err) => return HttpResponse::InternalServerError().body(err.to_string())
+    };
+
+    match send_notification(&client, device_token.as_str()).await {
         Ok(_) => HttpResponse::Ok().json(ApiResponse { data: "Notification sent.".to_string() }),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
