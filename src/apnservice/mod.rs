b7a80e8d0c6516eaeab3160c00e38b7e8e9d5a3b
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 19:10:50 -0300

    chore: refactor send notification to receive client

--- src/apnservice/mod.rs
+++ src/apnservice/mod.rs
@@ -2,43 +2,21 @@ use std::fs::File;
 use std::env;
 use a2::{Client, DefaultNotificationBuilder, Endpoint, NotificationBuilder, NotificationOptions};
 use log::{error, info};
+use std::error::Error;
 
-pub async fn send_notification(device_token: &str) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    const KEY_FILE: &str = "auth-key.p8"; // path to .p8 key file
-
-    let key_id = env::var("KEY_ID").expect("KEY_ID must be set");
-    let team_id = env::var("TEAM_ID").expect("TEAM_ID must be set");
+pub async fn send_notification(
+    client: &Client,
+    device_token: &str
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let topic = Some(env::var("BUNDLE_ID").expect("BUNDLE_ID must be set"));
 
-    let message = "Message from Rust!"; // Notification message
-    let endpoint = Endpoint::Sandbox; // APNs environment 
-
-    let private_key = match File::open(KEY_FILE) {
-        Ok(file) => file,
-        Err(e) => {
-            error!("Failed to open key file: {}", e);
-            return Err(e.into());
-        }
-    };
-
-    info!("Key file: {:?}", private_key);
-    info!("Key id: {}", key_id);
-    info!("Topic: {:?}", topic);
-    info!("Team id: {}", team_id);
-
-    let client = match Client::token(private_key, key_id, team_id, endpoint) {
-        Ok(client) => client,
-        Err(e) => {
-            error!("Failed to create client: {}", e);
-            return Err(e.into());
-        }
-    };
-    
     let options = NotificationOptions {
         apns_topic: topic.as_deref(),
         ..Default::default()
     };
     
+    let message = "Message from Rust!"; // Notification message
+
     // Notification payload
     let builder = DefaultNotificationBuilder::new()
         .set_body(message.as_ref())
@@ -54,6 +32,35 @@ pub async fn send_notification(device_token: &str) -> Result<(), Box<dyn std::er
         }
         Err(e) => {
             error!("Failed to send notification: {}", e);
+            Err(e.into())
+        }
+    }
+}
+
+pub fn setup_client() -> Result<Client, Box<dyn Error>> {
+    const KEY_FILE: &str = "auth-key.p8"; // path to .p8 key file
+
+    let key_id = env::var("KEY_ID").expect("KEY_ID must be set");
+    let team_id = env::var("TEAM_ID").expect("TEAM_ID must be set");
+    
+    let endpoint = Endpoint::Sandbox; // APNs environment 
+
+    let private_key = match File::open(KEY_FILE) {
+        Ok(file) => file,
+        Err(e) => {
+            error!("Failed to open key file: {}", e);
+            return Err(e.into())
+        }
+    };
+
+    info!("Key file: {:?}", private_key);
+    info!("Key id: {}", key_id);
+    info!("Team id: {}", team_id);
+
+    match Client::token(private_key, &key_id, &team_id, endpoint) {
+        Ok(client) => Ok(client),
+        Err(e) => {
+            error!("Failed to create client: {}", e);
             Err(e.into())
         }
     }
