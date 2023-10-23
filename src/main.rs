d3757066f3adbdc054e762289a16aaf763363238
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 19:22:10 -0300

    chore: inject client from main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod api;
 mod apnservice;
 
-use std::env;
+use std::{env, sync::Arc};
+use apnservice::setup_client;
 use dotenv::dotenv;
-use actix_web::{ App, HttpServer };
+use actix_web::{ App, HttpServer, web::Data };
 use api::trigger;
+use log::error;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -13,9 +15,20 @@ async fn main() -> std::io::Result<()> {
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let address = format!("0.0.0.0:{}", port);
 
-    HttpServer::new(|| {
+    let client = match setup_client() {
+        Ok(client) => client,
+        Err(err) => {
+            error!("Failed to setup client on error: {}", err);
+            std::process::exit(1)
+        },
+    };
+
+    let client = Arc::new(client);
+
+    HttpServer::new(move || {
         App::new()
-            .service(trigger)
+        .app_data(Data::new(client.clone()))
+        .service(trigger)
     })
     .bind(address)?
     .run()
