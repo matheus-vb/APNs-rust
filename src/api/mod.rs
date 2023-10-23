2305f69aa73d362b1d73eda627e2e8c94098ad2e
Matheus Batista <matheus.vbatista04@gmail.com>
2023-10-23 00:37:05 -0300

    feature: create api handler

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,14 +1,16 @@
-use actix_web::{HttpResponse, Responder};
+use actix_web::{web, HttpResponse, Responder, get};
 use serde::Serialize;
+use crate::apnservice::send_notification;
 
 #[derive(Serialize)]
-struct Message {
-    message: String,
+struct ApiResponse {
+    data: String,
 }
 
-pub async fn handle() -> impl Responder {
-    let hello_message = Message {
-        message: "Hello, world!".to_string(),
-    };
-    HttpResponse::Ok().json(hello_message)
+#[get("/trigger/{device_token}")]
+pub async fn trigger(device_token: web::Path<String>) -> impl Responder {
+    match send_notification(device_token.as_str()).await {
+        Ok(_) => HttpResponse::Ok().json(ApiResponse { data: "Notification sent.".to_string() }),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
 }
